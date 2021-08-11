6388fed1682618747aed1d357b013172f866c30c
Rockbesst Win <rockbesst@gmail.com>
2021-08-11 10:18:03 +0300

    disable user data

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2" {
 	count = var.i
 	availability_zone = var.avail_zones[count.index]
 	vpc_security_group_ids = var.sec_group_id
-	user_data = file("ready_webserver.sh")
+	#user_data = file("ready_webserver.sh")
 	key_name = var.ssh_key
 	associate_public_ip_address = var.allow_public_ip
 	tags = merge(var.tags, {Name = "WebServer"})
