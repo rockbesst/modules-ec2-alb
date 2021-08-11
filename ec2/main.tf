903132bd19b227080c2a6061811430de1892dcf3
Rockbesst Win <rockbesst@gmail.com>
2021-08-11 09:58:51 +0300

    fix

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "ec2" {
 	ami = data.aws_ami.amazon_linux.id
 	instance_type = var.instance_type
-	count = var.i
-	for_each = var.avail_zones
-		availability_zone = each.var.avail_zones
+	for_each = var.i
+		availability_zone = var.avail_zones[each]
 	vpc_security_group_ids = var.sec_group_id
 	user_data = file("ready_webserver.sh")
 	key_name = var.ssh_key
 	associate_public_ip_address = var.allow_public_ip
-	tags = merge(var.tags, {Name = "WebServer1"})
+	tags = merge(var.tags, {Name = "WebServer"+str(var.i)})
 }
 
 data "aws_ami" "amazon_linux" {
